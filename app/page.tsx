fb5223fefb3f3e470c8dab07b2a1370b1d240971
Imtiaz Shawn <imtiazshawn01@gmail.com>
2024-11-05 19:00:51 +0600

    Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button variant='default' size='lg' className='capitalize'>
+        Click Me
+      </Button>
+    </div>
+  )
+}
+
+export default HomePage;
