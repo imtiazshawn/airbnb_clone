001436b6e591480eb231f5ba6f9ea08b1e92b84b
Imtiaz Shawn <imtiazshawn01@gmail.com>
2024-12-18 22:50:39 +0600

    User Image

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -3,8 +3,12 @@ import FormContainer from '@/components/form/FormContainer';
 import FormInput from '@/components/form/FormInput';
 import { SubmitButton } from '@/components/form/SubmitButtons';
 import { CreateProfileAction } from '@/utils/actions';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-const CreateProfilePage = () => {
+const CreateProfilePage = async () => {
+  const user = await currentUser();
+  if(user?.privateMetadata?.hasProfile) redirect('/');
   return (
     <section>
         <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,14 +1,76 @@
-'use server';
+"use server";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { profileSchema } from "./schemas";
+import db from "./db";
+import { redirect } from "next/navigation";
 
-export const CreateProfileAction = async (prevState: any, formData: FormData) => {
-    try {
-        const rawData = Object.fromEntries(formData);
-        const validatedFields = profileSchema.parse(rawData);
-        console.log(validatedFields);
-        return { message: 'profile created' }
-    } catch (error) {
-        console.log(error);
-        return { message: 'there was an error' }
-    }
-}
+export const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) throw new Error("You must be logged in to access this route");
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
+
+export const CreateProfileAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  try {
+    const user = await currentUser();
+    if (!user) throw new Error("Please login before creating Profile");
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+  } catch (error) {
+    return {
+      message: error instanceof Error ? error.message : "An error occured",
+    };
+  }
+  redirect("/");
+};
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+
+  return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) redirect("/profile/create");
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: "udpate profile action" };
+};
